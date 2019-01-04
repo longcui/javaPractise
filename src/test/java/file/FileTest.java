6d0ee39a2984b6747c746205097fd77e1ed830a6
long <long@origin.no>
2019-01-04 13:49:00 +0100

    add checker if it is windows system(because I add maven test for github)

--- src/test/java/file/FileTest.java
+++ src/test/java/file/FileTest.java
@@ -11,21 +11,31 @@ import java.net.URL;
  * Created by long on 14.06.2017.
  */
 public class FileTest {
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
 
     @Test
     public void testSpaceInDir() throws IOException, URISyntaxException {
-        File file = new File("C:\\Program Files\\Apache Software Foundation\\Tomcat 7.0");
-        for (File file1 : file.listFiles()) {
-            System.out.println(file1.getCanonicalPath());
-        }
+        if(OS.contains("win")) {
+            File file = new File("C:\\Program Files\\Apache Software Foundation\\Tomcat 7.0");
+            for (File file1 : file.listFiles()) {
+                System.out.println(file1.getCanonicalPath());
+            }
 
-        URL url = file.toURI().toURL();
-        System.out.println(url.toString());
+            URL url = file.toURI().toURL();
+            System.out.println(url.toString());
+        } else {
+            throw new IOException("mock IO exception since it is not a windows sys");
+        }
     }
 
     @Test(expected = IOException.class)     //access is denied
     public void testWritePermission() throws IOException {
-        File file = new File("C:\\price_.xlsx");
-        file.createNewFile();
+        if(OS.contains("win")) {
+            File file = new File("C:\\price_.xlsx");
+            file.createNewFile();
+        } else {
+            throw new IOException("mock IO exception since it is not a windows sys");
+        }
     }
 }
