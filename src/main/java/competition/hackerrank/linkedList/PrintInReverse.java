f8f39da2d2efb384e8683319e594bcf3ee046117
long <long@origin.no>
2019-08-27 10:23:04 +0200

    good way to use Recursive

---
+++ src/main/java/competition/hackerrank/linkedList/PrintInReverse.java
@@ -0,0 +1,91 @@
+package competition.hackerrank.linkedList;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ * https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list-in-reverse/problem
+ *
+ *
+ * Tips:  best way to use recursive, think of stack
+ */
+public class PrintInReverse {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.print(sep);
+            }
+        }
+    }
+
+    // Complete the reversePrint function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void reversePrint(SinglyLinkedListNode head) {
+        if(head != null) {
+            reversePrint(head.next);
+            System.out.println(head.data);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list  = new SinglyLinkedList();
+        list.insertNode(1);
+        list.insertNode(2);
+        list.insertNode(3);
+        reversePrint(list.head);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+}
