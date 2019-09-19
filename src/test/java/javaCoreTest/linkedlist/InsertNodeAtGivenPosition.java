de7d8ace3f4d32c892f01754f85778c29485b84a
long <long@origin.no>
2019-09-19 13:05:59 +0200

    v0

---
+++ src/test/java/javaCoreTest/linkedlist/InsertNodeAtGivenPosition.java
@@ -0,0 +1,89 @@
+package javaCoreTest.linkedlist;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class InsertNodeAtGivenPosition {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtPosition function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+        SinglyLinkedListNode tmp = head;
+        SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+
+        for (int i = 0; i < position - 1; i++) {
+            tmp = tmp.next;
+        }
+
+        newNode.next = tmp.next;
+
+        if (position > 0) {
+            tmp.next = newNode;
+        } else {
+            //insert at position 0
+            newNode.next = head;
+            return newNode;
+        }
+
+        return head;
+    }
+}
+
