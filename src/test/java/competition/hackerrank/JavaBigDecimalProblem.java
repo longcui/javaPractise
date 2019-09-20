9631f6057bc334e31c0ba19d4f6f6a01687cd665
long <long@origin.no>
2019-09-20 13:03:11 +0200

    build a string to solve the problem

--- src/test/java/competition/hackerrank/JavaBigDecimalProblem.java
+++ src/test/java/competition/hackerrank/JavaBigDecimalProblem.java
@@ -5,37 +5,46 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class JavaBigDecimalProblem {
 
     @Test
     public void test() {
 //        String[] strings = {"-100", "50", "0", "56.6", "90", "0.12", ".12", "02.34", "000.000"};
-        String[] strings = {"0", "0", "0", "0", "0",};
+        String[] strings = {"0", "0", "0", "0", "0"};
         String[] returned = sort(strings);
         System.out.println(Arrays.toString(returned));
     }
 
     private static String[] sort(String []s) {
-        HashMap<String, Integer> stringIntegerHashMap = new HashMap<>();
+        HashSet<String> enhancedInput = new HashSet<>();
         for (int i = 0; i < s.length; i ++) {
             String s1 = s[i];
             if(s1 != null && !s1.isEmpty()) {
-                stringIntegerHashMap.put(s1, i);
+                enhancedInput.add(s[i] + ", " + i);
             }
         }
 
         TreeSet<String> treeSet = new TreeSet<String>((String o1, String o2) -> {
-            int compare = new BigDecimal(o1).compareTo(new BigDecimal(o2));
+            String[] splitO1 = o1.split(", ");
+            String[] splitO2 = o2.split(", ");
+            int compare = new BigDecimal(splitO1[0]).compareTo(new BigDecimal(splitO2[0]));
             if(compare == 0) {
-                return stringIntegerHashMap.get(o1).compareTo(stringIntegerHashMap.get(o2));
+                return Integer.compare(Integer.parseInt(splitO1[1]), Integer.parseInt(splitO2[1]));
             } else {
                 return -compare;
             }
         });
-        for (String s1 : stringIntegerHashMap.keySet()) {
+
+        for (String s1 : enhancedInput) {
                 treeSet.add(s1);
         }
-        return treeSet.toArray(new String [treeSet.size()]);
+
+        List<String> as = treeSet.stream()
+                .map(value -> (String)value.split(", ")[0])
+                .collect(Collectors.toList());
+
+        return as.toArray(new String [as.size()]);
     }
 }
