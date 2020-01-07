a655b469b7efa803f2f711eb7be8aa696dc5b722
long Cui <longcui@longs-MacBook-Pro-2019.local>
2020-01-07 14:25:05 +0100

    https://www.hackerrank.com/challenges/a-very-big-sum/problem

---
+++ src/main/java/java8/calculation/Calculation.java
@@ -0,0 +1,13 @@
+package java8.calculation;
+
+import java.util.Arrays;
+import java.util.stream.LongStream;
+
+public class Calculation {
+
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(final long[] ar) {
+        Arrays.stream(ar)
+
+    }
+}
