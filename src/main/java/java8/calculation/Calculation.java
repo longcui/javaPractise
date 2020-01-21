816af8efa59f211889d0d33366914cea59162b99
longcui <iamlongcui@gmail.com>
2020-01-21 10:13:51 +0100

    done

--- src/main/java/java8/calculation/Calculation.java
+++ src/main/java/java8/calculation/Calculation.java
@@ -1,6 +1,7 @@
 package java8.calculation;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.LongStream;
 
 public class Calculation {
@@ -11,4 +12,28 @@ public class Calculation {
         return longStream.sum();
 
     }
+
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+        int size = arr.size();
+        int backwardSlackSum = 0;
+        int slackSum = 0;
+
+        for(int i = 0; i < size; i ++) {
+            backwardSlackSum += arr.get(i).get(i);
+            slackSum += arr.get(i).get(size - i - 1);
+        }
+
+        return Math.abs(backwardSlackSum - slackSum);
+
+    }
 }
+
